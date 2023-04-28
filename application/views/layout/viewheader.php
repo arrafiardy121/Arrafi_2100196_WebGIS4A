8f2fdeb7a10afa8106883d6a396d727b47bf30ff
arrafiardy121 <arrafiardy0@upi.edu>
2023-04-28 17:49:05 +0700

    Langkah 18: Menambahkan base url kepada kode di viewheader.php

--- application/views/layout/viewheader.php
+++ application/views/layout/viewheader.php
@@ -4,8 +4,8 @@
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
     <div class="container">
-      <a href="../../index3.html" class="navbar-brand">
-        <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <a href="<?=base_url()?>assets/template/index3.html" class="navbar-brand">
+        <img src="<?=base_url()?>assets/template/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">AdminLTE 3</span>
       </a>
 
