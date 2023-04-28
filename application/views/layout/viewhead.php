ebe4a17850249ac1f92e2c3bd55719879374c13b
arrafiardy121 <arrafiardy0@upi.edu>
2023-04-28 17:37:59 +0700

    Langkah 13: Copy dan Paste Koding top-nav (JQuery) ke viewhead.php

--- application/views/layout/viewhead.php
+++ application/views/layout/viewhead.php
@@ -16,3 +16,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- Theme style -->
   <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
 </head>
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="../../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../../dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../../dist/js/demo.js"></script>
