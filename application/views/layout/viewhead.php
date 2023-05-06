dd19b4e87dde461150907398b979eac470466f0a
arrafiardy121 <arrafiardy0@upi.edu>
2023-05-06 16:10:23 +0700

    Langkah 3 (Leaflet.Coordinates): Menginstall plugin

--- application/views/layout/viewhead.php
+++ application/views/layout/viewhead.php
@@ -43,5 +43,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/L.Control.ZoomBar.css"/> 
 <script type="text/javascript" src="<?=base_url()?>assets/L.Control.ZoomBar.js"></script>
 
+<script type="text/javascript" src="<?=base_url()?>assets/Leaflet.Coordinates-0.1.5.min.js">
+</script> <link rel="stylesheet" href="<?=base_url()?>assets/Leaflet.Coordinates-0.1.5.css"/>
+
 
 </head>
