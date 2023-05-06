8e3b59e069f5b7e95273b9635ab902ad0b5469a7
arrafiardy121 <arrafiardy0@upi.edu>
2023-05-06 08:59:33 +0600

    Langkah 5(leaflet-minimap): konfigurasi plugin leaflet-minimap

--- application/views/peta_leaflet.php
+++ application/views/peta_leaflet.php
@@ -17,4 +17,13 @@ var baseLayers = {'Google Satellite Hybrid': GoogleSatelliteHybrid};
 var overlayLayers = {} 
 L.control.layers(baseLayers, overlayLayers, {collapsed: false}).addTo(map);
 
+var 
+osmUrl='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z} /{y}/{x}'; 
+var osmAttrib='Map data &copy; OpenStreetMap contributors'; 
+var osm2 = new L.TileLayer(osmUrl, {minZoom: 0, maxZoom: 13, attribution: osmAttrib }); 
+var rect1 = {color: "#ff1100", weight: 3}; 
+var rect2 = {color: "#0000AA", weight: 1, opacity:0, fillOpacity:0}; 
+var miniMap = new L.Control.MiniMap(osm2, {toggleDisplay: true, position : "bottomright", 
+aimingRectOptions : rect1, shadowRectOptions: rect2}).addTo(map);
+
 </script>
