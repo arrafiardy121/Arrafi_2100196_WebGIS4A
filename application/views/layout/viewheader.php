67ce455ddc5e8892ab1a716358586742f9a41eae
arrafiardy121 <arrafiardy0@upi.edu>
2023-05-06 18:58:50 +0700

    Langkah 1 (BAB 6): mengubah warna di file vewheader.php

--- application/views/layout/viewheader.php
+++ application/views/layout/viewheader.php
@@ -2,7 +2,7 @@
 <div class="wrapper">
 
   <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
+  <nav class="main-header navbar navbar-expand-md navbar-light navbar-dark">
     <div class="container">
       <a href="<?=base_url()?>assets/template/index3.html" class="navbar-brand">
         <img src="<?=base_url()?>assets/template/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
