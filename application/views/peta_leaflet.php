3a4482f53260396b441edd1909d70a34536641b8
arrafiardy121 <arrafiardy0@upi.edu>
2023-05-06 16:15:18 +0700

    Langkah 5 (Leaflet.Coordinates): Mengonfigurasi posisi leaflet.coordinates

--- application/views/peta_leaflet.php
+++ application/views/peta_leaflet.php
@@ -63,4 +63,15 @@ timeout: 10000
 
 var zoom_bar = new L.Control.ZoomBar({position: 'topleft'}).addTo(map);
 
+L.control.coordinates({ 
+position:"bottomleft", 
+decimals:2, 
+decimalSeperator:",", 
+labelTemplateLat:"Latitude: {y}",
+labelTemplateLng:"Longitude: {x}" 
+}).addTo(map); 
+/* scala */ 
+L.control.scale({metric: true, position: "bottomleft"}).addTo(map);
+
+
 </script>
