e1395069cff63d71d104e4ab37a9935a289e57fd
arrafiardy121 <arrafiardy0@upi.edu>
2023-05-16 09:07:46 +0700

    Langkah 7.1: Menambah Basemap (NatGeoWorldMap dan Menambah Koding BaseLayer Baru)

--- application/views/peta_leaflet.php
+++ application/views/peta_leaflet.php
@@ -14,7 +14,16 @@ maxZoom: 22,
 attribution: 'Latihan Web GIS' 
 }).addTo(map);
 
-var baseLayers = {'Google Satellite Hybrid': GoogleSatelliteHybrid}; 
+var Esri_NatGeoWorldMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
+attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
+maxZoom: 16
+});
+
+var baseLayers = { 
+    'Google Satellite Hybrid': GoogleSatelliteHybrid, 
+    'Esri_NatGeoWorldMap':Esri_NatGeoWorldMap 
+};
+
 var overlayLayers = {} 
 L.control.layers(baseLayers, overlayLayers, {collapsed: true}).addTo(map);
 
